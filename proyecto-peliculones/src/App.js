d2774079d3a83be42617278b3e30bf021d4b1b29
Moavro <tmoavro@udesa.edu.ar>
2023-08-28 17:12:25 -0300

    empezando el proyecto

---
+++ proyecto-peliculones/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "./componentes/Nabvar"
+import Footer from "./componentes/Footer"
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <main>
+
+      </main>
+      <Footer> </Footer>
+    </div>
+  );
+}
+
+export default App;
