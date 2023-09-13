53ceae8a51b801f7c2023eb2a9a39bb34c46395c
NicolasEhrman <nehrman@udesa.edu.ar>
2023-09-13 00:18:01 -0300

    footer

--- proyecto-peliculones/src/componentes/Footer/Footer.js
+++ proyecto-peliculones/src/componentes/Footer/Footer.js
@@ -2,9 +2,30 @@ import React from "react";
 
 function Footer(){
     return(
-        <footer> 
-            <p> footer de base militar </p>
-        </footer>
+            <footer className = "footer">
+                <div className = "texto_footer">
+                    <nav>
+                        <ul>
+                            <li className = "lista_footer"><a href="#" className = "items_footer">Términos y condiciones.</a></li>
+                            <li className = "lista_footer"><a href="#" className = "items_footer">Información legal.</a></li>
+                            <li>Todos los derechos reservados.</li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="logo_footer">
+                    <img  src="img/logo_peliculones.png" alt="logo" className="logo"></img>
+                    <h1 className="tituloprincipal">PELICULONES</h1>
+                    <img src ='img/tmdb.png' alt = 'logo tmdb' className="logotmdb"></img>
+                </div>
+                <div className = "integrantes"> 
+                    <section classNae = "items_integrantes">
+                        <article className = "lista_footer"><u>Developers</u></article>
+                        <article className = "lista_footer">Tomás Moavro</article>
+                        <article className = "lista_footer">Nicolás Ehrman</article>
+                        <article className = "lista_footer">Nicolas Vecchione</article>
+                    </section>
+                </div>
+            </footer>
     )
 }
 
