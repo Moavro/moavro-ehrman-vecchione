e85b088b6aaf6340bfbfa475205580e20287ebcd
NicoVecchione <nvecchione@udesa.edu.ar>
2023-09-13 13:53:37 -0300

    Avance

--- proyecto-peliculones/src/componentes/Series/Series.js
+++ proyecto-peliculones/src/componentes/Series/Series.js
@@ -1,15 +1,28 @@
 import React, {Component} from "react";
+import "./Series.css"
+
 
 class Series extends Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
             value: ""
         }
     }
-
+    componentDidMount(){
+    }
+    
     render(){
+        console.log(this.props.datosPeli)
 
+        return(
+            <React.Fragment>
+            <article className="peliculasjs">
+                <h3 className='peliculasjs'>{this.props.datosPeli.title}</h3>
+                <img className="pelis" src={`https://image.tmdb.org/t/p/w500${this.props.fotoPeli}`} />
+            </article>
+            </React.Fragment>
+        )
     }
 }
 
